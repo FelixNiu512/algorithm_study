3fb54d3aec95dd32ec1680e495ad275546b7d09c
Niu Chaohua <niuchaohua@foxmail.com>
2019-12-30 17:26:21 +0800

    init

---
+++ algorithms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  algorithms
+//
+//  Created by Constant Cody on 12/30/19.
+//  Copyright © 2019 Constant Cody. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
