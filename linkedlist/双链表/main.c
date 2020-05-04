08a7c7f364c4da942c5e15b17e31f5397913f8c9
Niu Chaohua <niuchaohua@foxmail.com>
2020-05-04 18:32:15 +0800

    结构调整

---
+++ linkedlist/双链表/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  双链表
+//
+//  Created by Constant Cody on 5/4/20.
+//  Copyright © 2020 Constant Cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
