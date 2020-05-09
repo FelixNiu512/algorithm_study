22e7c2ac27ac202b51d95f43d4632c652ec609cf
Niu Chaohua <niuchaohua@foxmail.com>
2020-05-09 17:01:22 +0800

    循环链表

---
+++ linkedlist/单向循环链表/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  单向循环链表
+//
+//  Created by Constant Cody on 5/9/20.
+//  Copyright © 2020 Constant Cody. All rights reserved.
+//
+
+#include <stdio.h>
+#include "stdlib.h"
+
+#define MAXSIZE 20
+
+/// 操作链表时返回的状态
+typedef enum LINKLIST_STATUS {
+    LINKLIST_STATUS_FAILURE = 0,
+    LINKLIST_STATUS_SUCCESS
+} Status;
+
+typedef int NodeDataType;
+
+typedef struct Node {
+    NodeDataType data;
+    struct Node *next;
+    int size;
+} Node, *LinkList;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
---
+++ linkedlist/双链表/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  双链表
+//
+//  Created by Constant Cody on 5/9/20.
+//  Copyright © 2020 Constant Cody. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
