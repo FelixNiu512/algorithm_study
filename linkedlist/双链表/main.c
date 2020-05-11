11240ee6633d445d342074541232e14c6f6618b9
Niu Chaohua <niuchaohua@foxmail.com>
2020-05-11 10:21:43 +0800

    双链表的其他操作补全

--- linkedlist/双链表/main.c
+++ linkedlist/双链表/main.c
@@ -79,7 +79,92 @@ Status list_insert(LinkList L, int i, NodeDataType data) {
     return LINKLIST_STATUS_SUCCESS;
 }
 
+// 删除
+Status list_delete(LinkList L, int i) {
+    // 合法性判断
+    if (L == NULL || i < 0 || i >= L->size) return LINKLIST_STATUS_FAILURE;
+    
+    // 寻找目标位置的前一个结点
+    LinkList preNode = L;
+    int j = 0;
+    while (j < i) {
+        preNode = preNode->next;
+        ++j;
+    }
+    
+    // 要删除的结点
+    LinkList delNode = preNode->next;
+    preNode->next = delNode->next;
+    // 如果delNode不是尾结点
+    if (delNode->next) delNode->next->prior = preNode;
+    free(delNode);
+    
+    // 更新结点数
+    L->size--;
+    return LINKLIST_STATUS_SUCCESS;
+}
+
+// 获取指定位置的数据
+Status list_getData(LinkList L, int i, NodeDataType *data) {
+    // 合法性判断
+    if (L == NULL || i < 0 || i >= L->size) return LINKLIST_STATUS_FAILURE;
+    
+    // 从首元结点开始遍历，直到 j == i
+    LinkList node = L->next;
+    int j = 0;
+    while (j < i) {
+        node = node->next;
+        ++j;
+    }
+    
+    // 此时node就是指定位置的结点
+    *data = node->data;
+    return LINKLIST_STATUS_SUCCESS;
+}
+
+// 清空链表
+Status list_empty(LinkList L) {
+    // 合法性判断
+    if (L == NULL) return LINKLIST_STATUS_FAILURE;
+    
+    LinkList delNode = L->next;
+    while (delNode) {
+        L->next = delNode->next;
+        free(delNode);
+        delNode = L->next;
+    }
+    
+    // 最后更新结点数
+    L->size = 0;
+    return LINKLIST_STATUS_SUCCESS;
+}
+
+// 销毁
+Status list_destroy(LinkList *L) {
+    if (*L == NULL) return LINKLIST_STATUS_FAILURE;
+    if ((*L)->size > 0) list_empty(*L);
+    // 释放头结点
+    free(*L);
+    *L = NULL;
+    return LINKLIST_STATUS_SUCCESS;
+}
+
 int main(int argc, const char * argv[]) {
     
+    LinkList L;
+    list_init(&L);
+    for (int i = 0; i < 10; i++) {
+        list_insert(L, i, 10+i);
+    }
+    list_print(L);
+    
+    list_delete(L, 1);
+    list_print(L);
+    
+    list_empty(L);
+    list_print(L);
+    
+    list_destroy(&L);
+    list_print(L);
     return 0;
 }
